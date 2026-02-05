4dc11213d11cbe697978433c70f102b504930889
jermainechogo-ship-it <jermainechogo@gmail.com>
2026-02-05 00:46:21 -0800

    Add files via upload

--- lesson1c.py
+++ lesson1c.py
@@ -4,3 +4,40 @@ number1 = 10
 number2 = 20
 addition = number1 + number2
 print("the sum of the numbers is: ", addition)
+
+
+# subbtraction
+number3 = 50
+number4 = 15
+diffrence = number3 -  number4
+print("The difference of the numbers is:", diffrence)
+
+# multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("the product of the numbers is:",product)
+
+# division
+# number7 and number8
+number7 = 40
+number8 = 5
+divide = number7 / number8
+print("The division of the numbers is:",divide)
+
+
+# modules - This means the remainder of a number when get subdivided by another one
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the numbers is:", modulus)
+
+# Floor division - This is the number of times  a number subdivides another ignoring the rimeinder
+floor = number9 // number10
+print("the floor division of the two numbers is:",floor)
+
+# Exponet (**)
+# The power of a number
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is:", exponent)
---
+++ lesson1d.py
@@ -0,0 +1,20 @@
+number1 = 5000
+number2 = 5/100
+number3 = 8
+simpleinterst = number1 * number2 * number3
+print("The simpleinterest is:", simpleinterst)
+
+# assignment
+# write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI=(weight) / (hieght squared)
+number4 = 78
+number5 = 1.75
+BMI =  number4 / number5 ** 2
+print("The BMI is:", BMI)
+
+# 2Find the area and perimeter of a rectangle whose lenght is 48cm and width is 25cm
+number6 = 48
+number7 = 15
+area = number6 * number7
+print("The area is:", area)
+perimeter = (number6 + number7) * 2
+print("The perimeter is:", perimeter)
